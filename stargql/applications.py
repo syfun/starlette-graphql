64ab4d49008173c8626b148bea13ebc29261c405
syfun <sunyu418@gmail.com>
2020-05-12 12:25:52 +0800

    feat: add custom error formater

--- stargql/applications.py
+++ stargql/applications.py
@@ -1,11 +1,12 @@
 import json
+import traceback
 import typing
 
 from gql.build_schema import build_schema, build_schema_from_file
 from gql.playground import PLAYGROUND_HTML
 from gql.resolver import default_field_resolver, register_resolvers
 from gql.utils import place_files_in_operations
-from graphql import GraphQLSchema, format_error, graphql
+from graphql import GraphQLError, GraphQLSchema, graphql
 from starlette import status
 from starlette.applications import Starlette
 from starlette.background import BackgroundTasks
@@ -15,6 +16,8 @@ from starlette.routing import BaseRoute, Route, WebSocketRoute
 from starlette.types import Receive, Scope, Send
 
 from .subscription import Subscription
+
+ERROR_FORMATER = typing.Callable[[GraphQLError], typing.Dict[str, typing.Any]]
 
 
 class GraphQL(Starlette):
@@ -29,6 +32,7 @@ class GraphQL(Starlette):
         path: str = '/',
         subscription_path: str = '/',
         subscription_authenticate: typing.Awaitable = None,
+        error_formater: ERROR_FORMATER = None,
         **kwargs,
     ):
         routes = routes or []
@@ -42,22 +46,60 @@ class GraphQL(Starlette):
 
         routes.extend(
             [
-                Route(path, ASGIApp(self.schema, playground=playground)),
-                WebSocketRoute(subscription_path, Subscription(self.schema, authenticate=subscription_authenticate)),
+                Route(
+                    path,
+                    ASGIApp(
+                        self.schema,
+                        debug=debug,
+                        playground=playground,
+                        error_formater=error_formater,
+                    ),
+                ),
+                WebSocketRoute(
+                    subscription_path,
+                    Subscription(self.schema, authenticate=subscription_authenticate),
+                ),
             ]
         )
         super().__init__(debug=debug, routes=routes, **kwargs)
 
 
 class ASGIApp:
-    def __init__(self, schema: GraphQLSchema, playground: bool = True) -> None:
+    def __init__(
+        self,
+        schema: GraphQLSchema,
+        debug: bool = False,
+        playground: bool = True,
+        error_formater: ERROR_FORMATER = None,
+    ) -> None:
         self.schema = schema
         self.playground = playground
+        self.error_formater = self.format_error
+        self.debug = debug
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         request = Request(scope, receive=receive, send=send)
         response = await self.handle_graphql(request)
         await response(scope, receive, send)
+
+    def format_error(self, error: GraphQLError) -> typing.Dict[str, typing.Any]:
+        if not error:
+            raise ValueError("Received null or undefined error.")
+        formatted = dict(  # noqa: E701 (pycqa/flake8#394)
+            message=error.message or "An unknown error occurred.",
+            locations=[l._asdict() for l in error.locations] if error.locations else None,
+            path=error.path,
+        )
+        if self.debug and error.original_error:
+            original_error = error.original_error
+            exception = error.extensions.get('exception', {})
+            exception['traceback'] = traceback.format_exception(
+                type(original_error), original_error, original_error.__traceback__
+            )
+            error.extensions['exception'] = exception
+        if error.extensions:
+            formatted.update(extensions=error.extensions)
+        return formatted
 
     async def handle_graphql(self, request: Request) -> Response:
         if request.method in ('GET', 'HEAD'):
@@ -85,20 +127,27 @@ class ASGIApp:
                     files_map = json.loads(form.get('map', '{}'))
                 except (TypeError, ValueError):
                     return PlainTextResponse(
-                        'operations or map sent invalid JSON', status_code=status.HTTP_400_BAD_REQUEST,
+                        'operations or map sent invalid JSON',
+                        status_code=status.HTTP_400_BAD_REQUEST,
                     )
                 data = place_files_in_operations(operations, files_map, form)
             else:
-                return PlainTextResponse('Unsupported Media Type', status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,)
+                return PlainTextResponse(
+                    'Unsupported Media Type', status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+                )
         else:
-            return PlainTextResponse('Method Not Allowed', status_code=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return PlainTextResponse(
+                'Method Not Allowed', status_code=status.HTTP_405_METHOD_NOT_ALLOWED
+            )
 
         try:
             query = data['query']
             variables = data.get('variables')
             operation_name = data.get('operationName')
         except KeyError:
-            return PlainTextResponse('No GraphQL query found in the request', status_code=status.HTTP_400_BAD_REQUEST,)
+            return PlainTextResponse(
+                'No GraphQL query found in the request', status_code=status.HTTP_400_BAD_REQUEST,
+            )
 
         background = BackgroundTasks()
         context = {'request': request, 'background': background}
@@ -111,8 +160,8 @@ class ASGIApp:
             context_value=context,
             field_resolver=default_field_resolver,
         )
-        error_data = [format_error(err) for err in result.errors] if result.errors else None
+        error_data = [self.error_formater(err) for err in result.errors] if result.errors else None
         response_data = {'data': result.data, 'errors': error_data}
-        status_code = status.HTTP_400_BAD_REQUEST if result.errors else status.HTTP_200_OK
+        # status_code = status.HTTP_400_BAD_REQUEST if result.errors else status.HTTP_200_OK
 
-        return JSONResponse(response_data, status_code=status_code, background=background)
+        return JSONResponse(response_data, status_code=status.HTTP_200_OK, background=background)
